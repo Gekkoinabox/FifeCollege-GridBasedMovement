4e029d395d75a4a34e554b981af2b2664536f9ea
Gekkoinabox <36261451+Gekkoinabox@users.noreply.github.com>
2018-06-15 14:09:11 +0100

    Finished
    Boulder sliding is still buggy as frick

--- Engine.cpp
+++ Engine.cpp
@@ -118,16 +118,27 @@ void Engine::setLevel(int currentLevel)
 		m_Grid.SetObject(0, 0, new Player(TextureHolder::GetTexture("graphics/player_down_01.png")));
 
 		// create exit
-		m_Grid.SetObject(9, 9, new GridSprite(TextureHolder::GetTexture("graphics/exit_locked.png"), GridObject::EXIT));
+		m_Grid.SetObject(9, 7, new GridSprite(TextureHolder::GetTexture("graphics/exit_locked.png"), GridObject::EXIT));
 
 		// create diamonds
-		m_Grid.SetObject(1, 4, new GridSprite(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
-		m_Grid.SetObject(1, 6, new GridSprite(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
-		m_Grid.SetObject(3, 2, new GridSprite(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(1, 7, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(2, 2, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(7, 4, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(9, 1, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(13, 1, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(11, 7, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
 
 		// create boulders
-		m_Grid.SetObject(1, 3, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
-		m_Grid.SetObject(7, 7, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(0, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(0, 7, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(3, 1, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(3, 2, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(10, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(10, 7, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(11, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(13, 2, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(14, 1, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(14, 2, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
 
 		// Fill the rest of our grid with dirt
 		for (int x = 0; x < m_Grid.GRID_SIZE_X; ++x)
@@ -140,5 +151,156 @@ void Engine::setLevel(int currentLevel)
 		}
 	}
 
+	if (currentLevel == 3)
+	{
+		// Clear the grid
+		for (int x = 0; x < m_Grid.GRID_SIZE_X; ++x)
+		{
+			for (int y = 0; y < m_Grid.GRID_SIZE_Y; ++y)
+			{
+
+				m_Grid.SetObject(x, y, nullptr, true);
+			}
+		}
+		// create player
+		m_Grid.SetObject(10, 0, new Player(TextureHolder::GetTexture("graphics/player_down_01.png")));
+
+		// create exit
+		m_Grid.SetObject(11, 7, new GridSprite(TextureHolder::GetTexture("graphics/exit_locked.png"), GridObject::EXIT));
+
+		// create diamonds
+		m_Grid.SetObject(0, 3, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(2, 6, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(6, 6, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(8, 1, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(12, 1, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(12, 5, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+
+		// create boulders
+		m_Grid.SetObject(1, 7, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(2, 7, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(5, 1, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(6, 1, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(6, 2, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(7, 1, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(10, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(10, 7, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(12, 2, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(12, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(12, 7, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+
+		// Fill the rest of our grid with dirt
+		for (int x = 0; x < m_Grid.GRID_SIZE_X; ++x)
+		{
+			for (int y = 0; y < m_Grid.GRID_SIZE_Y; ++y)
+			{
+				if (m_Grid.GetOjbect(x, y) == nullptr)
+					m_Grid.SetObject(x, y, new GridSprite(TextureHolder::GetTexture("graphics/dirt.png"), GridObject::DIRT));
+			}
+		}
+	}
+
+	if (currentLevel == 4)
+	{
+		// Clear the grid
+		for (int x = 0; x < m_Grid.GRID_SIZE_X; ++x)
+		{
+			for (int y = 0; y < m_Grid.GRID_SIZE_Y; ++y)
+			{
+
+				m_Grid.SetObject(x, y, nullptr, true);
+			}
+		}
+		// create player
+		m_Grid.SetObject(3, 7, new Player(TextureHolder::GetTexture("graphics/player_down_01.png")));
+
+		// create exit
+		m_Grid.SetObject(7, 1, new GridSprite(TextureHolder::GetTexture("graphics/exit_locked.png"), GridObject::EXIT));
+
+		// create diamonds
+		m_Grid.SetObject(1, 5, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(1, 7, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(6, 2, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(9, 7, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(13, 1, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(13, 2, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+
+		// create boulders
+		m_Grid.SetObject(1, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(5, 2, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(5, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(10, 7, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(12, 2, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(12, 3, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(14, 1, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+
+		// Fill the rest of our grid with dirt
+		for (int x = 0; x < m_Grid.GRID_SIZE_X; ++x)
+		{
+			for (int y = 0; y < m_Grid.GRID_SIZE_Y; ++y)
+			{
+				if (m_Grid.GetOjbect(x, y) == nullptr)
+					m_Grid.SetObject(x, y, new GridSprite(TextureHolder::GetTexture("graphics/dirt.png"), GridObject::DIRT));
+			}
+		}
+	}
+
+	if (currentLevel == 5)
+	{
+		// Clear the grid
+		for (int x = 0; x < m_Grid.GRID_SIZE_X; ++x)
+		{
+			for (int y = 0; y < m_Grid.GRID_SIZE_Y; ++y)
+			{
+
+				m_Grid.SetObject(x, y, nullptr, true);
+			}
+		}
+		// create player
+		m_Grid.SetObject(0, 6,new Player(TextureHolder::GetTexture("graphics/player_down_01.png")));
+
+		// create exit
+		m_Grid.SetObject(13, 7, new GridSprite(TextureHolder::GetTexture("graphics/exit_locked.png"), GridObject::EXIT));
+
+		// create diamonds
+		m_Grid.SetObject(0, 3, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(0, 7, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(6, 5, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(6, 7, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(7, 1, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(10, 1, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(11, 5, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+		m_Grid.SetObject(12, 4, new FallingObject(TextureHolder::GetTexture("graphics/diamond.png"), GridObject::DIAMOND));
+
+		// create boulders
+		m_Grid.SetObject(0, 4, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(1, 1, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(1, 7, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(6, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(7, 2, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(7, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(11, 2, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(11, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(12, 5, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(12, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(12, 7, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+		m_Grid.SetObject(13, 6, new FallingObject(TextureHolder::GetTexture("graphics/boulder.png"), GridObject::BOULDER));
+
+		// Fill the rest of our grid with dirt
+		for (int x = 0; x < m_Grid.GRID_SIZE_X; ++x)
+		{
+			for (int y = 0; y < m_Grid.GRID_SIZE_Y; ++y)
+			{
+				if (m_Grid.GetOjbect(x, y) == nullptr)
+					m_Grid.SetObject(x, y, new GridSprite(TextureHolder::GetTexture("graphics/dirt.png"), GridObject::DIRT));
+			}
+		}
+	}
+	if (currentLevel == 6)
+	{
+		m_Window.close();
+	}
+
+	m_Grid.changedExit = false;
 	m_CurrentLevel = currentLevel;
 }
--- Update.cpp
+++ Update.cpp
@@ -17,13 +17,22 @@ void Engine::update(float dtAsSeconds)
 	}
 	if (m_Grid.CheckForDiamonds() == 0 && m_Grid.changedExit == false)
 	{
-		if( m_CurrentLevel == 1)
+		if(m_CurrentLevel == 1)
 			m_Grid.SetObject(14, 9, new GridSprite(TextureHolder::GetTexture("graphics/exit_open.png"), GridObject::EXIT));
+		if(m_CurrentLevel == 2)
+			m_Grid.SetObject(9, 7, new GridSprite(TextureHolder::GetTexture("graphics/exit_open.png"), GridObject::EXIT));
+		if (m_CurrentLevel == 3)
+			m_Grid.SetObject(11, 7, new GridSprite(TextureHolder::GetTexture("graphics/exit_open.png"), GridObject::EXIT));
+		if (m_CurrentLevel == 4)
+			m_Grid.SetObject(7, 1, new GridSprite(TextureHolder::GetTexture("graphics/exit_open.png"), GridObject::EXIT));
+		if (m_CurrentLevel == 5)
+			m_Grid.SetObject(13, 7, new GridSprite(TextureHolder::GetTexture("graphics/exit_open.png"), GridObject::EXIT));
 		m_Grid.changedExit = true;
 	}
 	if (m_Grid.CheckForExit() == 0)
 	{
 		Engine::setLevel((Engine::m_CurrentLevel) + 1);
+		m_Grid.changedExit = false;
 	}
 	
 		
